d63668de49454e2fb505d5b1388341899fe0286f
inixon <nikita@listratov.com>
2014-03-30 01:14:51 +0300

    Update jquery.inputsum.js

--- jquery.inputsum.js
+++ jquery.inputsum.js
@@ -8,29 +8,29 @@
  
  HowTo
  1. HTML MarkUp
- Very simple. Just add some checbox with value and same class. Don't forget "result" area in code.
+ Very simple. Just add some checkbox with value and same class. Don't forget "result" area in code.
  
- <div type="checkbox" class="myclass" value="100" />
- <div type="checkbox" class="myclass" value="150" />
- <div type="checkbox" class="myclass" value="210" />
- ....
- <div type="checkbox" class="myclass" value="5500" />
+   <div type="checkbox" class="myclass" value="100" />
+   <div type="checkbox" class="myclass" value="150" />
+   <div type="checkbox" class="myclass" value="210" />
+     ....
+   <div type="checkbox" class="myclass" value="5500" />
  
- Price: <span id="result">0</span>
+   Price: <span id="result">0</span>
  
  2.jQuery
- Just call function as usual
+ Call function as usual
  
- $('.myclass').inputsum();
+   $('.myclass').inputsum();
  
- And after click on chekboxs you will see summ in "result" area on page.
+ And after click on chekbox you will see change summ in "result" area on page.
  
  3.Settings
  Plugin have just one settings. If you want change name for result code, just call function with key 'result', like this
  
- $('.myclass').inputsum({
-   result: '#another-id'
- });
+   $('.myclass').inputsum({
+     result: '#another-id'
+   });
  
  
 */
