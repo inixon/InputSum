0b5c8ebac7d4046183d9b39199dfdea89b0bb6d5
inixon <nikita@listratov.com>
2014-03-30 00:30:18 +0300

    Update jquery.inputsum.js

--- jquery.inputsum.js
+++ jquery.inputsum.js
@@ -1,3 +1,10 @@
+/*!
+ * jQuery Input Sum Plugin
+ * version: 0.1.a (30-mar-2014)
+ * @requires jQuery v1.10 or later
+ * Licensed under the MIT licenses: http://malsup.github.com/mit-license.txt
+ */
+
 (function( $ ) {
     $.fn.inputsum = function(options) {
         
