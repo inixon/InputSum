eaaec6128466c153289235976573c99106e445b7
inixon <nikita@listratov.com>
2014-03-30 01:20:58 +0300

    Update jquery.inputsum.js
    Change plugin version to 1.0

--- jquery.inputsum.js
+++ jquery.inputsum.js
@@ -1,6 +1,6 @@
 /*
  jQuery Input Sum Plugin
- Version: 0.1.a (30-mar-2014)
+ Version: 1.0 (30-mar-2014)
  Author: Nikita Listratov
  URL: https://github.com/inixon/InputSum
  Requires jQuery v1.10 or later
@@ -14,7 +14,7 @@
    <input type="checkbox" class="myclass" value="150" />
    <input type="checkbox" class="myclass" value="210" />
      ....
-   <div type="checkbox" class="myclass" value="5500" />
+   <input type="checkbox" class="myclass" value="5500" />
  
    Price: <span id="result">0</span>
  
@@ -26,7 +26,7 @@
  And after click on chekbox you will see change summ in "result" area on page.
  
  3.Settings
- Plugin have just one settings. If you want change name for result code, just call function with key 'result', like this
+ Plugin have just one settings. If you want change name for result code, just call function with key 'result' like this
  
    $('.myclass').inputsum({
      result: '#another-id'
