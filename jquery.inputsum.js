549048e7f48c574dfac18db76957c0845d84bc95
inixon <nikita@listratov.com>
2014-03-30 00:57:36 +0300

    Update jquery.inputsum.js

--- jquery.inputsum.js
+++ jquery.inputsum.js
@@ -1,11 +1,29 @@
-/*!
- * jQuery Input Sum Plugin
- * version: 0.1.a (30-mar-2014)
- * Author: Nikita Listratov
- * URL: https://github.com/inixon/InputSum
- * @requires jQuery v1.10 or later
- * Licensed under the MIT licenses: http://malsup.github.com/mit-license.txt
- */
+/*
+ jQuery Input Sum Plugin
+ Version: 0.1.a (30-mar-2014)
+ Author: Nikita Listratov
+ URL: https://github.com/inixon/InputSum
+ Requires jQuery v1.10 or later
+ Licensed under the MIT licenses
+ 
+ HowTo
+ 1. HTML MarkUp
+ Very simple. Just add checbox with value and same class. Don't forget "result" div
+ 
+ <div type="checkbox" class="myclass" value="100" />
+ <div type="checkbox" class="myclass" value="100" />
+ <div type="checkbox" class="myclass" value="100" />
+ 
+ <div id="result"></div>
+ 
+ 2.jQuery
+ Just call function as usual
+ 
+ $('.myclass').inputsum();
+ 
+ And after clicking on chekbox you will see summ in "result" div.
+ 
+*/
 
 (function( $ ) {
     $.fn.inputsum = function(options) {
