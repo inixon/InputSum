d4a0006e38dd11cb01b5faeabfe1d4a60f6c8688
inixon <nikita@listratov.com>
2014-03-30 03:46:02 +0400

    Update version to 1.0.2
    - fix to proper class detection
    - fix namespace for settings
    - fix HowTo

--- jquery.inputsum.js
+++ jquery.inputsum.js
@@ -1,14 +1,16 @@
 /*
  jQuery Input Sum Plugin
- Version: 1.0.1 (30-mar-2014)
+ Version: 1.0.2 (30-mar-2014)
  Author: Nikita Listratov
  URL: https://github.com/inixon/InputSum
  Requires jQuery v1.10 or later
  Licensed under the MIT licenses
  
  HowTo
+ Plugin is very simple to use.
+ 
  1. HTML MarkUp
- Very simple. Just add some checkbox with value and same class. Don't forget "result" area in code.
+ Just add some checkbox with value and same class. Don't forget "result" area in code.
  
    <input type="checkbox" class="myclass" value="100" />
    <input type="checkbox" class="myclass" value="150" />
@@ -23,13 +25,25 @@
  
    $('.myclass').inputsum();
  
- And after click on chekbox you will see change summ in "result" area on page.
+ And after click on chekbox you will see change sum in "result" area on page.
  
  3.Settings
- Plugin have just one settings. If you want change name for result code, just call function with key 'result' like this
+ Plugin have two settings. 
+   
+   3.1 Result container
+   By default result container must have ID 'is-result'.
+   If you want change name for result container just call function with option 'result'.
  
    $('.myclass').inputsum({
      result: '#another-id'
+   });
+   
+   3.2 Specify reset button
+   By default reset button must have ID 'is-reset'. 
+   If you have another name just call function with option 'reset'.
+ 
+   $('.myclass').inputsum({
+     reset: '#another-id'
    });
  
  
@@ -39,12 +53,13 @@
     $.fn.inputsum = function(options) {
         
         var settings = $.extend({
-            result: '#result',
-            reset: '#reset'
+            result: '#is-result',
+            reset: '#is-reset'
         }, options );
+       
+       var $this = $(this); 
         
-        
-       $(':checkbox').on('click', function() {
+       $( 'input.'+$this.attr('class')+':checkbox' ).on('click', function() {
         
          if ($(this).is(':checked')) {
             
@@ -66,7 +81,7 @@
        
        $(settings.reset).on('click', function() {
            $(settings.result).html('0');
-           $(':checkbox').attr("checked", false);
+           $('input.'+$this.attr('class')+':checkbox').attr("checked", false);
        });
     }
 }( jQuery ));
